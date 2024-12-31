3d794101f388d0eeb8c2915c62a5cf03d5a3bb3c
Abdalkarim Razzouk <xme3v16@gmail.com>
2024-12-31 23:38:21 +0300

    New

---
+++ app/Http/Controllers/CommentController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Comment;
+use App\Models\Task;
+use Illuminate\Support\Facades\Auth;
+
+
+class CommentController extends Controller
+{
+    public function store(Request $request, Task $task)
+    {
+        $request->validate([
+            'content' => 'required|string',
+        ]);
+
+        $task->comments()->create([
+            'content' => $request->content,
+            'user_id' => Auth::id(),
+        ]);
+
+        return back()->with('success');
+    }
+}
